fb61db62dd3df217ea514e4c5f460429f576d052
haojichen <42987077+haojichen@users.noreply.github.com>
2020-06-29 22:48:45 +0800

    root
    陈浩基

---
+++ 2.0（new_upload/Root/root.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>管理员登入</title>
+    <link rel="stylesheet" href="../CSS/forguanli.css">
+</head>
+<body>
+<h1 align="center">后台管理系统</h1>
+<div class="middle">
+    <div class="button1">
+        <a href="view_user.php"><input class="one" type="button" value="查看用户信息"></a>
+        <a href="change_userinfo.php"><input class="one" type="button" value="修改用户信息"></a>
+    </div>
+    <div class="button2">
+        <a href="delete_user.php"><input class="two" type="button"  value="删除用户"></a>
+        <a href="root_register.php"><input class="two" type="button" value="新增用户"></a>
+    </div>
+
+
+</div>
+</body>
+</html>
